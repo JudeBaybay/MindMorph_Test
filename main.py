1fd1df56d947bef6417bf609f0ae152d265899fa
cnstntnhanna <constantinohanna23@gmail.com>
2025-11-13 11:48:11 +0800

    commit

--- main.py
+++ main.py
@@ -68,5 +68,3 @@ def create_learningstyle(id: int, learning: updatelearningstyleSchema):
 
     if not learning.data:
         return {"message": "No record found or update failed"}
-    
-    return {"message": "Learning style updated successfully", "data": learning.data}
